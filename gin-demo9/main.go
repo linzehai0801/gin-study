acc7b78aeaa222a35d0b071be73281a03eb69915
lin <1444968177@qq.com>
2022-12-08 16:34:32 +0800

    获取path参数

---
+++ gin-demo9/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/user/search/:username/:address", func(c *gin.Context) {
+		username := c.Param("username")
+		address := c.Param("address")
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "ok",
+			"username": username,
+			"address":  address,
+		})
+	})
+	r.Run()
+}
+
+//浏览器访问http://127.0.0.1:8080/user/search/aaa/bbb
